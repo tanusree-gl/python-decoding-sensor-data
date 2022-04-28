ec7d9b671038c70b4888e0575ee524dd7ae23f39
Tanusree Roy <tanusree.roy@globallogic.com>
2022-04-28 16:16:25 +0530

    Final commit

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -4,11 +4,11 @@ from load_data import load_sensor_data
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
-data = load_sensor_data()
 print("Sensor Data App")
 ##############################
 
 # Module 1 code here:
+data = load_sensor_data()
 print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
 

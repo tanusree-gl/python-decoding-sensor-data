a1d97a1feba4938ba7c863dc8da6481566a7a018
Tanusree Roy <tanusree.roy@globallogic.com>
2022-04-28 16:40:34 +0530

    Completed module

---
+++ sensor/house_info.py
@@ -0,0 +1,15 @@
+from datetime import date
+class HouseInfo(object):
+    def __init__(self,data):
+        self.data = data
+    def get_data_by_area(self,field,rec_area = 0):
+        field_data = list()
+        for record in self.data:
+            if rec_area == 0:
+                field_data.append(record[field])
+            elif rec_area == int(record['area']):
+                field_data.append(record[field])
+        return field_data
+    def get_data_by_date(self, field, rec_date = date.today()):
+        field_data = []
+        

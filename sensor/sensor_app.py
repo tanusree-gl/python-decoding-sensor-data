d9abcd05cf23ea786091961707d6f6d5bdfe8965
Tanusree Roy <tanusree.roy@globallogic.com>
2022-05-19 13:53:13 +0530

    Completed Humidity Module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -2,6 +2,8 @@
 from load_data import load_sensor_data
 from house_info import HouseInfo 
 from temperature_info import TemperatureData
+from humidity_info import  HumidityData
+from statistics import mean
 from datetime import date,datetime
 ##############################
 # Do not remove these two lines
@@ -30,5 +32,11 @@ print("\nHouse Temperature sensor records for date: {} = {}".format(
     test_date.strftime("%m/%d/%y"), len(recs)))
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 # Module 4 code here:
-
+humidity_data = HumidityData(data)
+recs = humidity_data.get_data_by_area(rec_area = test_area)
+print("\nHouse Humidity sensor records for area {} = {}".format(test_area, len(recs)))
+print("\tAverage: {} humidity".format(mean(recs)))
+recs = humidity_data.get_data_by_date(rec_date = test_date)
+print("House Humidity sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
+print("\tAverrage: {} humidity".format(mean(recs)))
 # Module 5 code here:

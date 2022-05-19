995c335884c33b0ddba8436f58faa6a76c3506fc
Tanusree Roy <tanusree.roy@globallogic.com>
2022-05-19 12:08:53 +0530

    modified

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -18,12 +18,11 @@ house_info = HouseInfo(data)
 test_area = 1
 recs = house_info.get_data_by_area("id",rec_area = test_area)
 print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
-# Module 3 code here:
 test_date = datetime.strptime("5/9/20","%m/%d/%y")
 recd = house_info.get_data_by_date("id",rec_date = test_date)
 print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recd)))
+# Module 3 code here:
+
 # Module 4 code here:
 
 # Module 5 code here:
-
-# Module 6 code here:

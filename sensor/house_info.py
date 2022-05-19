2e76a6d103ecaa33af08f5f4924786ea244fdab4
Tanusree Roy <tanusree.roy@globallogic.com>
2022-05-19 16:11:19 +0530

    completed energy

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -3,7 +3,7 @@ class HouseInfo(object):
     def __init__(self,data):
         self.data = data
     def get_data_by_area(self,field,rec_area = 0):
-        field_data = list()
+        field_data = []
         for record in self.data:
             if rec_area == 0:
                 field_data.append(record[field])

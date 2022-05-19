c6af95741dc054f23a2c6d617d79c33555d0dff8
Tanusree Roy <tanusree.roy@globallogic.com>
2022-05-19 13:32:19 +0530

    Completed Module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,6 +1,7 @@
 # Runner script for all modules
 from load_data import load_sensor_data
 from house_info import HouseInfo 
+from temperature_info import TemperatureData
 from datetime import date,datetime
 ##############################
 # Do not remove these two lines
@@ -20,7 +21,14 @@ test_date = datetime.strptime("5/9/20","%m/%d/%y")
 recd = house_info.get_data_by_date("id",rec_date = test_date)
 print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recd)))
 # Module 3 code here:
-
+temperature_data = TemperatureData(data)
+recs_temp = temperature_data.get_data_by_area(rec_area = test_area)
+print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs_temp)))
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs_temp), min(recs_temp)))
+recd_temp = temperature_data.get_data_by_date(rec_date = test_date)
+print("\nHouse Temperature sensor records for date: {} = {}".format(
+    test_date.strftime("%m/%d/%y"), len(recd_temp)))
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recd_temp), min(recd_temp)))
 # Module 4 code here:
 
 # Module 5 code here:

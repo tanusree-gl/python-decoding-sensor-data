3b60040aec9977d0d53ccb58fafaa35ebbd193bb
Tanusree Roy <tanusree.roy@globallogic.com>
2022-05-19 14:50:15 +0530

    modified

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -43,6 +43,8 @@ print("\tAverage: {} humidity".format(mean(recs)))
 recs = humidity_data.get_data_by_date(rec_date = test_date)
 print("House Humidity sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
 print("\tAverage: {} humidity".format(mean(recs)))
+
+# Module 5 code here:
 particle_data = ParticleData(data)
 recs = particle_data.get_data_by_area(rec_area = test_area)
 print("\nHouse Particle sensor records for area {} = {}".format(test_area, len(recs)))
@@ -50,11 +52,11 @@ concentrations = particle_data.get_data_concentrations(data = recs)
 print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+
+# Module 6 code here:
 recs = particle_data.get_data_by_date(rec_date = test_date)
 print("\nHouse Particle sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
 concentrations = particle_data.get_data_concentrations(data = recs)
 print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
-
-# Module 5 code here:

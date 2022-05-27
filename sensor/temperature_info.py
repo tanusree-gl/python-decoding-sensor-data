5a42219f46dd90290f368919dc5cd6cbc5a92340
Tanusree Roy <tanusree.roy@globallogic.com>
2022-05-27 11:51:25 +0530

    done

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -7,7 +7,7 @@ class TemperatureData(HouseInfo):
     def _convert_data(self,data):
         recs = []
         for rec in data:
-            recs.append(int(rec,10)) 
+            recs.append(int(rec,base = 10)) 
         return recs
     
     def get_data_by_area(self, rec_area = 0):
